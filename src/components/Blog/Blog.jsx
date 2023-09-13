16797722b23298cf1aef577ce5c9deb4582b00c1
Zilani <zilani.cse.cou@gmail.com>
2023-09-13 23:31:13 +0600

    bolg component reformed

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,11 +1,26 @@
 // import PropTypes from 'prop-types'; // ES6
 import PropTypes from 'prop-types';
 
-const Blog = ({blog}) => {
+const Blog = ({ blog }) => {
     console.log(blog);
+    const { id, cover, title, author, authorImg, postedDate, readingTime, hashTags } = blog;
     return (
-        <div>
-            
+        <div className='my-4'>
+            <img className='max-w-xl rounded-lg' src={cover} alt="" />
+            <div className='flex justify-between my-4'>
+                <div className='flex items-center gap-4'>
+                    <img className='w-16 h-16 rounded-full object-cover' src={authorImg} alt="" />
+                    <div>
+                        <p>{author}</p>
+                        <p>{postedDate}</p>
+                    </div>
+                </div>
+                <h4>{readingTime} min read</h4>
+            </div>
+            <h1 className='text-5xl'>{title}</h1>
+            {
+                hashTags.map((tag, idx) => <span key={idx}>   #{tag}   </span>)
+            }
         </div>
     );
 };
